89502bbd3b1ca02b228f5ebc8900ac83983edbe8
Timur <xanov.timur@mail.ru>
2023-06-16 22:10:32 +0300

    пуш

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите номер четверти: ");
+int a = int.Parse(Console.ReadLine());
+if (a < 1 || a > 4)
+{
+    Console.WriteLine("Недопустимое значение");
+    Environment.Exit(0);
+}
+switch (a)
+{
+case 1:
+    Console.WriteLine("Диапазон x > 0 и y > 0");
+    break;
+case 2:
+    Console.WriteLine ("Диапазон x < 0 и y > 0");
+    break;
+case 3:
+    Console.WriteLine ("Диапазон x < 0 и y < 0");
+    break;
+case 4:
+    Console.WriteLine ("Диапазон x > 0 и y < 0");
+    break;
+}
